2718bff1dd24da35cd76198b623d3edf81256eb0
jean-françois gaucheron <jean-francois_gaucheron@MacBook-Pro-de-jean-francois.local>
2016-03-20 09:13:51 +0100

    Redirecting special messages to a specific log file

--- src/main/java/net/javabeat/SpringBootLoggingDemo.java
+++ src/main/java/net/javabeat/SpringBootLoggingDemo.java
@@ -11,12 +11,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootLoggingDemo {
  
   private static final Logger logger = LoggerFactory.getLogger(SpringBootLoggingDemo.class);
+  private static final Logger special = LoggerFactory.getLogger("jfg.logger.special");
   
   public static void main(String[] args) {
     
     SpringApplication springApplication = new SpringApplication(new Object[]{SpringBootLoggingDemo.class});
     springApplication.run(args);
-    
+
+    special.info("This is a special MESSAGE");
+
     logger.error("Message logged at ERROR level");
     logger.warn("Message logged at WARN level {} {} {} {} {} {}", 1, 2, 3, 4, 5, "coucou");
     logger.info("Message logged at INFO level");
